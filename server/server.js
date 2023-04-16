4db3e9a2061fd1381987c3644f38930f3d2cf4d3
TORBI Mohammed <TORBI@Casablanca-bourse.com>
2023-04-16 01:40:26 +0000

    add log requests

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import exepress from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import { Configuration, OpenAIApi } from 'openai';
+import fs from 'fs';
 
 dotenv.config();
 
@@ -9,15 +10,58 @@ const configuration = new Configuration({
     apiKey: process.env.OPENIA_API_KEY,
 });
 
+function logAPICalls(req, res, next) {
+
+    const requestDate = new Date();
+    const requestMethod = req.method;
+    const requestUrl = req.originalUrl;
+    const requestBody = req.body;
+    const requestHeaders = req.headers;
+
+    res.on('finish', () => {
+        const responseDate = new Date();
+        const responseStatus = res.statusCode;
+        const responseBody = res.locals.data;
+
+        const logData = {
+            requestDate,
+            requestMethod,
+            requestUrl,
+            requestBody,
+            requestHeaders,
+            responseDate,
+            responseStatus,
+            responseBody,
+        };
+
+        const logDataString = JSON.stringify(logData);
+
+        console.log('API Response:', logDataString);
+
+        const year = responseDate.getFullYear();
+        const month = (responseDate.getMonth() + 1).toString().padStart(2, '0');
+        const day = responseDate.getDate().toString().padStart(2, '0');
+
+        fs.appendFile(`logs/${year}${month}${day}.log`, logDataString + '\n', (err) => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    });
+
+    next();
+}
+
 const openai = new OpenAIApi(configuration);
 
 const app = exepress();
 app.use(cors());
 app.use(exepress.json());
+app.use(logAPICalls);
 
 app.get('/', async(req, res) =>{
     res.status(200).send({
-        message: 'Hello from CodeX',
+        message: 'Hello from Education Simulator',
     })
 })
 
@@ -25,23 +69,15 @@ app.post('/', async(req, res) =>{
     try{
         const prompt = req.body.prompt;
 
-        /*const response = await openai.createCompletion({
-            "model": "text-davinci-003",
-            "prompt": `${prompt}`,
-            "temperature": 0.7,
-            "max_tokens": 1900,
-            "top_p": 1,
-            "frequency_penalty": 0.5,
-            "presence_penalty": 0
-        });*/
-
         const response = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
             messages: [{ role: "user", content: prompt }],
             max_tokens: 1900
             });
+
+        res.locals.data = response.data.choices[0].message.content;
         res.status(200).send({
-            bot: response.data.choices[0].message.content
+            bot: res.locals.data
         })
 
     }catch(error){
